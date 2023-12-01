48585654b9168cba42274bfd522b62bb446ce810
SERVOSA\becari1141 <jostinfabian.cabascango@servosa.com>
2023-12-01 16:39:31 +0100

    Add function to obtain third color using at() method

---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+// Función para obtener el tercer color de un array usando el método at()
+function obtenerTercerColor() {
+    let colores = ["Verde", "Rojo", "Amarillo", "Azul", "Negro", "Blanco"];
+    // Usar el método at() para obtener el tercer color
+    let tercerColor = colores.at(2);
+    return tercerColor;
+}
